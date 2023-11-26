9b6af99c07c4281f8958f3b89308f19e58afdb01
frogrammer9 <oskar@meler.pl>
2023-11-26 17:28:09 +0100

    brought up to date with main

--- DTL_dev/dtl.cpp
+++ DTL_dev/dtl.cpp
@@ -1,22 +1,28 @@
 #include"dtl.h"
-#include<time.h>
 
 dtl::Logger dtl::Logger::s_Log;
-namespace dtl
-{
-	dtl::Logger& Log = dtl::Logger::GetInstance();
-}
+dtl::Logger& dtl::Logger::GetInstance() { return s_Log; }
+namespace dtl { dtl::Logger& Log = dtl::Logger::GetInstance(); }
+void dtl::Logger::output(const std::string& text) { m_msg << text << '\n'; }
+void dtl::Logger::setFile(const std::string& filePath) { m_filepath = filePath; }
 
 bool dtl::Logger::findToken(const std::string& s, size_t* t1, size_t* t2)
 {
-	*t1 = s.find('{');
-	*t2 = s.find('}');
+	*t1 = s.find('{'); *t2 = s.find('}');
 	return (*t2 - *t1 == 2);
 }
 
-void dtl::Logger::output(const std::string& text) { m_msg << text << '\n'; }
+void dtl::Logger::processToken(char c, const std::string& arg)
+{
+	if (c == '0') m_msg << arg;
+	else if (c == 'b') { long long val; memcpy(&val, &arg, sizeof(arg)); m_msg << std::bitset<sizeof(arg) * 8>(val); }
+}
+void dtl::Logger::processToken(char c, const char* arg)
+{
+	if (c == '0') m_msg << arg;
+	else if (c == 'b') { long long val; memcpy(&val, &arg, sizeof(arg)); m_msg << std::bitset<sizeof(arg) * 8>(val); }
+}
 
-//todo Implement time zones
 void dtl::Logger::showtime()
 {
 	if (m_timeFormat == DTL_PROGRAM_TIME)
@@ -39,13 +45,12 @@ void dtl::Logger::showtime()
 }
 
 dtl::Logger::Logger()
-	:m_shouldstayopen(false), m_time_start(std::clock()), m_mode(0), m_colEntry(DTL_WHITE),
+	:m_time_start(std::clock()), m_mode(0), m_colEntry(DTL_WHITE),
 	m_colInfo(DTL_GREEN), m_colWarning(DTL_YELLOW), m_colError(DTL_RED), m_timeFormat(0)
 {}
 
 dtl::Logger::~Logger()
 {
-	if (m_shouldstayopen) system("PAUSE");
 	if (!m_msg.str().empty() && m_mode == DTL_FILE)
 	{
 		m_outf.open(m_filepath, std::ofstream::out);
@@ -57,8 +62,6 @@ dtl::Logger::~Logger()
 	}
 }
 
-dtl::Logger& dtl::Logger::GetInstance() { return s_Log; }
-
 void dtl::Logger::settings(std::string entryCol, std::string infoCol, std::string warningCol, std::string errorCol, int timeFormat, int logLocation)
 {
 	if (entryCol != "\0") m_colEntry = entryCol;
@@ -68,8 +71,6 @@ void dtl::Logger::settings(std::string entryCol, std::string infoCol, std::strin
 	m_timeFormat = timeFormat;
 	m_mode = logLocation;
 }
-
-void dtl::Logger::setFile(const std::string& filePath) { m_filepath = filePath; }
 
 dtl::Timer::Timer() {}
 dtl::Timer::~Timer() {}
